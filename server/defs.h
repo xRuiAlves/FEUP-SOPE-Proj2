bac09c6575a0639a50c11f0955cedbecbd49877f
Miguel Duarte <gamer-go@hotmail.com>
2018-05-10 00:32:48 +0100

    Updated defines file with description of some macros

--- server/defs.h
+++ server/defs.h
@@ -2,12 +2,12 @@
 #define _DEFS_H_
 
 // Error Macros
-#define MAX -1
-#define NST -2
-#define IID -3
-#define ERR -4
-#define NAV -5
-#define FUL -6
+#define MAX -1 /*Number of desired seats is bigger than MAX_CLI_SEATS*/
+#define NST -2 /*Invalid number of desired seats*/
+#define IID -3 /*Invalid desired seat identifier*/
+#define ERR -4 /*Other parameter error*/
+#define NAV -5 /*At least one of the desired seats cannot be placed, seat not available*/
+#define FUL -6 /*Room full*/
 
 // Server Data Macros
 #define MAX_ROOM_SEATS  9999
@@ -18,6 +18,9 @@
 
 //Delay in request processing simulation
 #include <unistd.h>
-#define DELAY() usleep(100);
+#define DELAY() usleep(200*1000);
+
+#define MAX_MESSAGE_SIZE 512
+#define CLOSED_WRITE_FIFO_WAIT_DELAY_MS 200
 
 #endif  // _DEFS_H_
