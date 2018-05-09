69fb23d4ddfc4fb594e5294ecddb59c0c4144bb7
xRuiAlves <up201606746@fe.up.pt>
2018-05-09 14:27:31 +0100

    Refactored request_message functions

--- client/client.c
+++ client/client.c
@@ -130,17 +130,6 @@ u_int parse_pref_seat_list(char* pref_seat_list, u_int* parsed_pref_seat_list, u
     return num_pref_seats;
 }
 
-RequestMessage create_request_message(u_int pid, u_int num_wanted_seats, u_int* pref_seat_list, u_int num_pref_seats) {
-    RequestMessage msg;
-
-    msg.pid = pid;
-    msg.num_wanted_seats = num_wanted_seats;
-    msg.pref_seat_list = pref_seat_list;
-    msg.num_pref_seats = num_pref_seats;
-
-    return msg;
-}
-
 void print_usage(FILE* stream) {
     fprintf(stream, "usage: client <time_out> <num_wanted_seats> <pref_seat_list>\n");
 }
--- client/request_message.c
+++ client/request_message.c
@@ -1,1 +1,14 @@
 #include "request_message.h"
+
+
+
+RequestMessage create_request_message(u_int pid, u_int num_wanted_seats, u_int* pref_seat_list, u_int num_pref_seats) {
+    RequestMessage msg;
+
+    msg.pid = pid;
+    msg.num_wanted_seats = num_wanted_seats;
+    msg.pref_seat_list = pref_seat_list;
+    msg.num_pref_seats = num_pref_seats;
+
+    return msg;
+}
